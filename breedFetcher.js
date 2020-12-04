b8207c9a6e8552f4b6e1d552bb855feda86116fd
Aleksa Gavrilovic <lks_g@live.ca>
2020-12-03 19:39:52 -0500

    refactored breedFetcher & created index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,19 @@
-let input = process.argv.slice(2);
-
 const request = require('request');
-request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (error, response, body) => {
-  if (error) console.log('error:', error);
-  //console.log('body:', body);
-  const data = JSON.parse(body);
-  if (data.length === 0) console.log('Data not found');
-  //const descr = data.description;
-  //console.log(data);
-});
 
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error);
+    } else {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback(error, 'Data not found');
+      } else {
+        let breedDescription = data[0].description;
+        callback(error, breedDescription);
+      }
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
